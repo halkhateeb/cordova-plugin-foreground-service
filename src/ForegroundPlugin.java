2e0ed7b7fb5f506ae2f64fadcd03e48db3ea8b87
halkhateeb <49714658+halkhateeb@users.noreply.github.com>
2019-09-04 12:54:55 +0300

    Remove API restrictions from the plugin file
    To support devices with lower API levels

--- src/ForegroundPlugin.java
+++ src/ForegroundPlugin.java
@@ -6,13 +6,13 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.annotation.TargetApi;
+// import android.annotation.TargetApi;
 
 public class ForegroundPlugin extends CordovaPlugin {
     @Override
-    @TargetApi(26)
+//     @TargetApi(26)
     public boolean execute (final String action, final JSONArray args, final CallbackContext command) throws JSONException {
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
+//         if (android.os.Build.VERSION.SDK_INT >= 26) {
             Activity activity = cordova.getActivity();
             Intent intent = new Intent(activity, ForegroundService.class);
 
@@ -36,7 +36,7 @@ public class ForegroundPlugin extends CordovaPlugin {
                 // Stop the service
                 activity.getApplicationContext().startService(intent);
             }
-        }
+//         }
 
         command.success();
         return true;
